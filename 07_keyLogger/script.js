06e50e266bbd3becfd9ee3fb30825e20731044eb
Rupesh Shah <124813194+rupeshkmrshah@users.noreply.github.com>
2024-05-08 20:02:41 +0000

    Initial Commit

---
+++ 07_keyLogger/script.js
@@ -0,0 +1,30 @@
+const logdiv = document.getElementById('log');
+const statediv = document.getElementById('state');
+const startbtn = document.getElementById('start-btn');
+const stopbtn = document.getElementById('stop-btn');
+
+startbtn.addEventListner("click", ()=>{
+    document.addEventListener('keydown', handleDown);
+    document.addEventListener('keyup', handleUp);
+    // startbtn.disabled = true;
+    // stopbtn.disabled = false;
+})
+
+stopbtn.addEventListner("click", ()=>{
+    document.removeEventListener('keydown', handleDown)
+    document.removeEventListener('keyup', handleUp)
+    logdiv.textContent = '';
+    statediv.textContent =  '';
+    // startbtn.disabled = false;
+    // stopbtn.disabled = true;
+})
+
+function handleDown(e) {
+    logdiv.textContent = `Key ${e.key} pressed down`;
+    statediv.textContent = `Key is Down`;
+}
+
+function handleUp(e) {
+    logdiv.textContent = `Key ${e.key} pressed up`;
+    statediv.textContent = `Key is Up`;
+}
