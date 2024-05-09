32b49fd5cceda5af22a50199f0b46bf3456873d6
Rupesh Shah <124813194+rupeshkmrshah@users.noreply.github.com>
2024-05-09 11:30:45 +0000

    Initial Commit

--- 07_keyLogger/script.js
+++ 07_keyLogger/script.js
@@ -3,20 +3,20 @@ const statediv = document.getElementById('state');
 const startbtn = document.getElementById('start-btn');
 const stopbtn = document.getElementById('stop-btn');
 
-startbtn.addEventListner("click", ()=>{
+startbtn.addEventListener("click", ()=>{
     document.addEventListener('keydown', handleDown);
     document.addEventListener('keyup', handleUp);
-    // startbtn.disabled = true;
-    // stopbtn.disabled = false;
+    startbtn.disabled = true;
+    stopbtn.disabled = false;
 })
 
-stopbtn.addEventListner("click", ()=>{
+stopbtn.addEventListener("click", ()=>{
     document.removeEventListener('keydown', handleDown)
     document.removeEventListener('keyup', handleUp)
     logdiv.textContent = '';
     statediv.textContent =  '';
-    // startbtn.disabled = false;
-    // stopbtn.disabled = true;
+    startbtn.disabled = false;
+    stopbtn.disabled = true;
 })
 
 function handleDown(e) {
